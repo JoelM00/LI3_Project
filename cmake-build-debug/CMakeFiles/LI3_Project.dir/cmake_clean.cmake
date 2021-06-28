1bede62bae379945e5bcdd2d5abdd97076c679b1
JoelM00 <61049184+JoelM00@users.noreply.github.com>
2021-06-28 08:43:21 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/LI3_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LI3_Project.dir/main.c.obj"
+  "LI3_Project.exe"
+  "LI3_Project.exe.manifest"
+  "LI3_Project.pdb"
+  "libLI3_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LI3_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
